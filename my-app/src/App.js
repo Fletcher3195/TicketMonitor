35aa382811e20e3306fe8980e6b4d76a2a9f3b85
James Fletcher <fletcher.3195@gmail.com>
2020-03-14 14:20:09 -0400

    Low effort copy of ticketmaster svg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,26 +7,18 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Row from 'react-bootstrap/Row';
 
 function App() {
   return (
     <Container fluid>
       <Navbar bg="dark" variant="dark" expand="lg">
-        <Navbar.Brand href="#home">Ticket Monitor</Navbar.Brand>
+        <Navbar.Brand href="#home"><i id="logo">ticketmonitor</i></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="https://github.com/Fletcher3195/TicketMonitor">Github</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
